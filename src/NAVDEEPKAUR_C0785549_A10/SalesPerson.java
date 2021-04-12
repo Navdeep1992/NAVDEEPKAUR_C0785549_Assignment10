bf9b8657776b971194a95b257934b22c48f4b336
Navdeep1992 <navk02200@gmail.com>
2021-04-12 19:59:30 -0400

    Initial commit

---
+++ src/NAVDEEPKAUR_C0785549_A10/SalesPerson.java
@@ -0,0 +1,24 @@
+package NAVDEEPKAUR_C0785549_A10;
+
+public abstract class SalesPerson {
+    double sales;
+    double advance;
+
+    public double getSales() {
+        return sales;
+    }
+
+    public void setSales(double sales) {
+        this.sales = sales;
+    }
+
+    public double getAdvance() {
+        return advance;
+    }
+
+    public void setAdvance(double advance) {
+        this.advance = advance;
+    }
+
+    abstract void display();
+}
